b329a0b0efbee62c0c384d548dacd317028b9a70
narradorww <rodrigo.anst@gmail.com>
2021-10-09 17:35:21 -0300

    Criado metodo POST

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,23 @@
 import express, {Request, Response, NextFunction} from 'express';
 import usersRoute from './routes/users.route';
+import { StatusCodes } from 'http-status-codes'
 
 const app = express();
 
+//Configurações da Aplicação
+//Midleware para ler o JSPN
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+
+
+// Configurações de Rotas
 app.use(usersRoute);
 
 app.get('/status', (req: Request, res: Response, nexT: NextFunction) => {
-    res.status(200).send({ foo : 'Sucesso no carregamento do server'});
+    res.status(StatusCodes.OK).send({ foo : 'Sucesso no carregamento do server'});
 } );
-
+//Inicialização do Server
 app.listen(3000, () => {
     console.log('Rodando na 3000')
 });
--- src/routes/users.route.ts
+++ src/routes/users.route.ts
@@ -1,11 +1,24 @@
 
 import { Router, Request, Response, NextFunction } from 'express';
+import { StatusCodes } from 'http-status-codes'
 
 const usersRoute =Router();
 
 usersRoute.get('/users', (req:Request, res:Response, next: NextFunction) =>{
     const users = [{userName: 'Rodrigo'}];
-    res.status(200).send({users})
+    res.status(StatusCodes.OK).send({users})
+});
+
+usersRoute.get('/users/:uuid', (req:Request <{uuid:string}>, res:Response, next: NextFunction) =>{
+    const uuid=req.params.uuid;
+    res.status(StatusCodes.OK).send({uuid})
+});
+
+
+usersRoute.post('/users',(req:Request , res:Response, next: NextFunction) =>{
+    const newUser = req.body;
+    console.log(req.body)
+    res.status(StatusCodes.CREATED).send(newUser)
 });
 
 export default usersRoute;
