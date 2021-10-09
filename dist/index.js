183d9e9ff2072e7f95da753f70d040c73795981c
narradorww <rodrigo.anst@gmail.com>
2021-10-09 14:34:32 -0300

    Instalada a biblioteca ts-node-dev para que detecte mudanças e as salve sem a necessidade de comando de transpilaçao!

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,13 @@
 "use strict";
-console.log('Hello Word!');
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const app = (0, express_1.default)();
+app.get('/status', (req, res, nexT) => {
+    res.status(200).send({ foo: 'bar' });
+});
+app.listen(3000, () => {
+    console.log('Rodando na 3000');
+});
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-console.log('Hello Word!')
+import express, {Request, Response, NextFunction} from 'express';
+
+
+const app = express();
+
+app.get('/status', (req: Request, res: Response, nexT: NextFunction) => {
+    res.status(200).send({ foo : 'bar'});
+} );
+
+app.listen(3000, () => {
+    console.log('Rodando na 3000')
+});
+
+
+
+
